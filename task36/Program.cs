6c0d3e2bebac6c6ff5b88a77b128ed1ab63cc78c
Kailar0000 <kailar0000@gmail.com>
2022-12-19 17:41:29 +0300

    Task36

---
+++ task36/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+}
+
+Console.Clear();
+int n = new Random().Next(1, 10);
+int[] array = new int[n];
+int sum = 0;
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+for (int i = 1; i < array.Length; i=i+2)
+{
+	sum= sum + array[i];
+}
+
+Console.WriteLine(sum);
