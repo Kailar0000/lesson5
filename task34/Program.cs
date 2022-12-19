50f75e705b20d2e8061b5236af8ee63ee6500ce9
Kailar0000 <kailar0000@gmail.com>
2022-12-19 17:34:11 +0300

    Task34

---
+++ task34/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+Console.Clear();
+int n = new Random().Next(1, 10);
+int[] array = new int[n];
+int num = 0;
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+{
+	if(array[i]%2 == 0)
+		num= num + 1;
+}
+
+Console.WriteLine($"Чётных чисел в массиве {num}");
